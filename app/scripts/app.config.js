d58af3c3240b4b22f74a5a68561f488dd7dd12b5
Harsh Mehta <hamehta3@gmail.com>
2016-01-10 00:06:24 -0500

    Replace hardcoded api values with env variables

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -2,9 +2,9 @@ angular.module('app.config', []);
 
 angular.module('app.config').factory('apiUrl', function($location) {
   // API Config
-  var protocol = 'https' //$location.protocol();
-  var host = 'aaa-drivinglaws-api.herokuapp.com' // $location.host();
-  var port = 443; // $location.port();
+  var protocol = process.env.API_PROTOCOL || $location.protocol();
+  var host = process.env.API_HOST || $location.host();
+  var port = process.env.API_PORT || $location.port();
   var fullApiUrl = function() {
     return protocol+"://"+host+":"+port+"/";
   };
