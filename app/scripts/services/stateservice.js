ae5cd7007a950853c4bf08ced7f75fa8170c6e4c
Harsh Mehta <hamehta3@gmail.com>
2018-06-21 22:09:51 -0400

    Cleanup log statement; fix travis config

--- app/scripts/services/stateservice.js
+++ app/scripts/services/stateservice.js
@@ -47,7 +47,7 @@ angular.module('drivingLawsDiffApp')
     }
 
     function processResponse(response) {
-      $log.info("Successfully returned response");
+      //$log.info("Successfully returned response");
       //$log.log(response);
       return response.data[0];
     }
